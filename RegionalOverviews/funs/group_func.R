e37de4d45243233d51e8860880f70d686f7d636d
Marta Suska <mr.ska@wp.pl>
2019-04-07 23:30:12 +0200

    Significant change in programming with dplyr - using rename instead of enquo... . Facets added (for multiannual)

--- RegionalOverviews/funs/group_func.R
+++ RegionalOverviews/funs/group_func.R
@@ -1,14 +1,18 @@
 group_func = function(df,
                       var,
                       groupBy,
+                      groupBy2 = NA,
+                      facet = NA,
                       func,
                       type_of_threshold = 'none',
                       value_of_threshold = NA,
                       Catch_group = NA) {
   # df - a data frame
-  # var -  a column to be summmarised e.g. var = OfficialLandingCatchWeight
-  # groupBy - names of columns, by which the grouping should be carried out. IMPORTANT to write it as groupBy = quos(...) e.g. groupBy = quos(Harbours, HarboursDesc)
-  # func - function summarising the data: sum,  n_distinct,  e.g. func = sum
+  # var -  a column to be summmarised e.g. var = 'OfficialLandingCatchWeight'
+  # groupBy - name of column, by which the grouping should be carried out. e.g. groupBy = 'Area'
+  # groupBy2 - name of second column for grouping, makes sense only for scatterpie maps. e.g. group2 = 'FlagCountry'
+  # facet - will be used for facet_wrap, e.g. facet = 'Year'
+  # func - function summarising the data: sum,  n_distinct,  e.g. func = 'sum'
   # type_of_threshold - default set to 'none', other options: 'top_n', 'percent'
   # value_of_threshold - set it, if you defined any type_of_threshold
   # Catch_group - if NA then all species will be included, other options: demersal/flatfish/smallpelagic/largepelagic
@@ -20,26 +24,42 @@ group_func = function(df,
   ################################################################################
   require(tidyverse)
   require(rlang)
+
   
-  var <- enquo(var)
+  # rename the columns
+  var_name = var
+  var <- as.symbol(var_name)
+  groupBy_name = groupBy
+  groupBy <- as.symbol(groupBy_name)
+  if(!is.na(facet)){
+    facet_name <- facet
+    facet = as.symbol(facet)  
+  }else{
+    facet = NA
+    facet_name = NA
+  }
   
-  func_name = quo_name(enquo(func))
-  var_name <- quo_name(var)
-  #col_name <- paste0(func_name, '_', var_name)
+  func_name = func#quo_name(enquo(func))
+  func = eval_tidy(as.symbol(func))
+  # # CHECK the data
+  # Check if the given groupBy variabl is present in the dataset
   
-  ################################################################################
-  # CHECK the data
+  if(!groupBy_name %in% colnames(df)) {
+    stop(paste(
+      'The given column --->',
+      groupBy_name,
+      '<--- is not present in the df dataset'
+    ))
+  }
   
-  # Check if the given groupBy variables are present in the dataset
-  lapply(groupBy, function(x) {
-    if (!quo_name(x) %in% colnames(df)) {
-      stop(paste(
-        'The given column --->',
-        quo_name,
-        '<--- is not present in the df dataset'
-      ))
-    }
-  })
+  if(!is.na(facet) & !facet_name %in% colnames(df)) {
+    stop(paste(
+      'The given column --->',
+      facet_name,
+      '<--- is not present in the df dataset'
+    ))
+  }
+  
   
   # Check if the given var variable is present in the dataset
   if (!var_name %in% colnames(df)) {
@@ -59,6 +79,14 @@ group_func = function(df,
     ))
   }
   
+  ################################################################################
+  
+  df %>% mutate(var = !!var,
+                groupBy = !!groupBy,
+                facet = !!facet) -> df
+  
+  ################################################################################
+  
   # If  func = sum-> var should be numeric. if it's not -> automatic conversion
   if (func_name %in% c('sum')) {
     sapply(df, is.numeric) %>% which %>% names %>%  intersect(var_name) %>% length -> isNumeric
@@ -73,14 +101,16 @@ group_func = function(df,
           It was automatically converted into numeric'
         )
         )
-      df %>% mutate(!!var_name := as.numeric(!!var)) -> df
+      df %>% mutate(var = as.numeric(var)) -> df
     }
-    df %>% mutate(!!var_name := as.numeric(!!var)) -> df # put it in both places to avoid error with integer overflow
+    df %>% mutate(var = as.numeric(var)) -> df # put it in both places to avoid error with integer overflow
     
   }
   
+  ################################################################################
+  
   # Inform about missing values
-  df %>% filter(is.na(!!var)) -> warn
+  df %>% filter(is.na(var)) -> warn
   if (nrow(warn) > 0) {
     message(paste('There are', nrow(warn), 'rows with missing', var_name, sep = " "))
   }
@@ -108,35 +138,31 @@ group_func = function(df,
     }
   }
   
-  # TEMPORARY SOLUTION AS I DON"T HAVE SPECIES GROUPS FUNCTION FROM FISH PI
-  # if(!is.na(Catch_group)){
-  #     df = df %>% filter(Species==Catch_group)
-  # }
+  df  %>% group_by(groupBy, facet) %>%  summarise(var = func(var, na.rm = TRUE),
+                                                  analysis_type = func_name) -> gdf
   
   
-  df  %>% group_by(!!!groupBy) %>%  summarise(!!var := func(!!var, na.rm = TRUE),
-                                              analysis_type = func_name) -> gdf
+  #save info about missing groupBy entries
+  gdf %>% ungroup() %>% group_by(facet) %>% mutate(pr = var/sum(var, na.rm = TRUE)*100) %>% filter(is.na(groupBy))->missing_entries
   
-  # save info about missing groupBy entries
-    gdf %>% ungroup() %>% mutate(pr = !!var/sum(!!var, na.rm = TRUE)*100) %>% filter(is.na(!!groupBy[[1]]))->missing_entries
-
-
+  
   # apply the threshold
   if (type_of_threshold == 'percent') {
     tdf = gdf %>% ungroup() %>%
-      arrange(desc(!!var)) %>%
-      mutate(pr := !!var / sum(!!var) * 100) %>%
+      group_by(facet) %>% 
+      arrange(desc(var)) %>%
+      mutate(pr := var / sum(var) * 100) %>%
       mutate(cum_pr = cumsum(pr)) %>%
-      filter(cum_pr <= value_of_threshold) %>% 
+      filter(cum_pr <= value_of_threshold) %>%
       mutate(type_of_threshold = type_of_threshold,
              value_of_threshold = value_of_threshold)
     
   } else if (type_of_threshold == 'top_n') {
-    tdf = gdf %>% ungroup() %>% arrange(desc(!!var)) %>% top_n(value_of_threshold,!!var)%>% 
+    tdf = gdf %>% ungroup() %>%group_by(facet) %>%  arrange(desc(var)) %>% top_n(value_of_threshold,var)%>%
       mutate(type_of_threshold = type_of_threshold,
              value_of_threshold = value_of_threshold)
   } else if (type_of_threshold == 'none') {
-    tdf = gdf %>%  ungroup()%>% arrange(desc(!!var))
+    tdf = gdf %>%  ungroup()%>% group_by(facet) %>% arrange(desc(var))
   } else{
     message(paste('No method  defined for threshold type = ', type_of_threshold))
     tdf = NULL
@@ -145,15 +171,25 @@ group_func = function(df,
   if(!is.na(Catch_group)){
     tdf = tdf %>% mutate(Catch_group = Catch_group)
   }
-    
+  
+  if(is.na(facet)){
+    tdf %>% select(-facet) %>% rename(!!var_name :=var, !!groupBy_name :=groupBy) %>% ungroup()-> tdf  
+  }else{
+    tdf %>% rename(!!var_name :=var, !!groupBy_name :=groupBy, !!facet_name := facet) %>% ungroup()-> tdf  
+  }
+  
   return(list(tdf = tdf, percent_missing =  missing_entries))
   
   }
 
+# # example
+# group_func(cl_rcg %>% filter(Year %in% c(2016, 2017)),
+#            'OfficialLandingCatchWeight',
+#            'Area',
+#            facet = 'Year',
+#            func = 'sum',
+#            type_of_threshold = 'top_n',
+#            value_of_threshold = 20,
+#            Catch_group = NA)
 
 
-# Examples:
-#group_func(CL_2014_NSEA, var = OfficialLandingValue,  groupBy=quos(FlagCountry), func = sum, type_of_threshold = 'none')
-#group_func(CL_2014_NSEA, var = OfficialLandingCatchWeight,  groupBy=quos(FlagCountry, LandingCountry), func = sum, type_of_threshold = 'percent',value_of_threshold = 90)
-#group_func(CL_2014_NSEA, var = FlagCountry,  groupBy=quos(LandingCountry), func = n_distinct, type_of_threshold = 'top_n', value_of_threshold = 10)
-#group_func(CL_2014_NSEA, var = OfficialLandingCatchWeight,  groupBy=quos(FlagCountry, LandingCountry), func = sum, type_of_threshold = 'percent', value_of_threshold =90)
