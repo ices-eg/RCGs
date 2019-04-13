b252e70b6a3bab63bef83ed845ce8d38f0792587
nmprista <31890653+nmprista@users.noreply.github.com>
2019-04-13 14:36:52 +0200

    Update func_download_data_from_sharepoint.r
    - fixed another bug when downloads very fast (small files); simplified code
    - added option to delete_zip
    - fixed bug in saving (was saving large files too fast, with size==0)

--- RegionalOverviews/funs/func_download_data_from_sharepoint.r
+++ RegionalOverviews/funs/func_download_data_from_sharepoint.r
@@ -1,4 +1,4 @@
- download_data_from_sharepoint<-function(sharepoint_address, filename_vector, dir_download_browser, dir_download_target, unzip)
+ download_data_from_sharepoint<-function(sharepoint_address, filename_vector, dir_download_browser, dir_download_target, unzip = FALSE, delete_zip = FALSE)
 	{
 		# Nuno Prista, SLU, 2019 (project PanRegional SubGroup on Fisheries and Sampling Overviews)
 			# improvement of function download_data_cs4, developed by Nuno Prista, IPMA, 2015 (project fishPi)
@@ -19,6 +19,9 @@
 
 		# Development notes
 			# 2019-04-03: fixed bug when downloads very fast (small files); simplified code
+			# 2019-04-13: fixed another bug when downloads very fast (small files); simplified code
+			# 2019-04-13: added option to delete_zip
+			# 2019-04-13: fixed bug in saving (was saving large files too fast, with size==0)
 
 			
 		# function for press key (from "https://stackoverflow.com/questions/15272916/how-to-wait-for-a-keypress-in-r")
@@ -28,11 +31,14 @@
 				line <- readline()
 			}
 		
+		# Note: for some reason if *png or *txt firefox opens automatically in browser and does not prompt for saving...
+			if(sum(grepl(filename_vector, pat=".txt") | grepl(filename_vector, pat=".png"))>0) stop("Sorry, not yet developed for '.txt' and '.png' files")
+		
 		# opens connection
 			browseURL(sharepoint_address)
 		
 		# waits for user and password
-			print("Press key when sharepoint is open")
+			print("Press key when sharepoint is open (you may need to login if you have not already)")
 			readkey()
 		
 		# loop to download files
@@ -42,29 +48,48 @@
 
 			# prompts download file to dir_download_browser
 				browseURL(paste(sharepoint_address, filename, sep="/"))
-
+	
+			Sys.sleep(5); print(".waiting 5 secs for download to start...")
 		
 			# waits for file to be fully downloaded
-				files<-list.files(dir_download_browser)
-				while(sum(grepl(files, pat=".part", fixed=T))>0 | !file.exists(paste(dir_download_browser, filename, sep="/"))) 
+				files<-list.files(dir_download_browser);
+				#while(sum(grepl(files, pat=".part", fixed=T))>0 |  file.info(paste(dir_download_browser, filename, sep="/"))$size) 
+				while(
+					(grepl(filename, pat=".zip", fixed=T) & sum(grepl(files, pat=".part", fixed=T))>0) |  
+						(!is.na(file.size(paste(dir_download_browser, filename, sep="/"))) & file.size(paste(dir_download_browser, filename, sep="/"))==0)) 
 					{
 					Sys.sleep(2); print(".waiting 2 more secs for download to finish...")
 					files<-list.files(dir_download_browser)
 					}
-					
-			print("Download finished!!")	
-		
+				print("Download finished!!")	
+				print(paste("final file_size:", file.size(paste(dir_download_browser, filename, sep="/"))))
+		# check
+			if(!file.exists(paste(dir_download_browser, filename, sep="/"))) stop ("file not downloaded!")
+			
 		# copies data from dir_download_browser to dir_download_target, removing from dir_download_browser
 			print(paste(".copying",filename,"from dir_download_browser to target.dir"))
 			file.copy(from = paste(dir_download_browser,filename,sep="/"), to = dir_download_target)
 			print(paste(".removing",filename,"from dir_download_browser"))
 			file.remove(paste(dir_download_browser, filename,sep="/"))
+		
+			print("Done!")		
+
+		if(unzip == TRUE)
+			{
+			if(grepl(filename, pat = ".zip", fixed=T))
+				{
+				Sys.sleep(2)
+				print(".unzipping...")
+	#			browser()
+				unzip(zipfile = paste(dir_download_target, filename, sep="/"), exdir=dir_download_target)
+				print("Done!")
+					if(delete_zip == TRUE)
+					{
+					print("Deleting zip...")
+					file.remove(paste(dir_download_target, filename,sep="/"))
+					print("Done!")
+					}
+				} else print("att: zip options ignored - file extension is not zip")
+			}
 		}
-		print("Done!")		
-	
-	if(unzip == TRUE)
-		{
-		unzip(zipfile = paste(dir_download_target, filename, sep="/"))
-		}
-	
 	}
