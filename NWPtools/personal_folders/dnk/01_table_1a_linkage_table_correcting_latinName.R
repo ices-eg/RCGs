498eb84352421b8e76db19eb72c1028a003c4891
KirstenBirchHaakansson <kih@aqua.dtu.dk>
2019-10-24 13:36:25 +0200

    Corrected, so spp fit names in the new file

--- NWPtools/personal_folders/dnk/01_table_1a_linkage_table_correcting_latinName.R
+++ NWPtools/personal_folders/dnk/01_table_1a_linkage_table_correcting_latinName.R
@@ -22,20 +22,20 @@ asfisNo<-subset(asfis,is.na(Scientific_name))
 #run spp program to find the correct species
 
 asfis$latinName_new<-ifelse(asfis$latinName=="Ammodytidae", "Ammodytes spp,Ammodytes tobianus", 
-                            ifelse(asfis$latinName=="Anarhichas spp.", "Anarhichas spp,Anarhichas minor,Anarhichas denticulatus,Anarhichas lupus", 
-                            ifelse(asfis$latinName=="Aphanopus spp.", "Aphanopus spp,Aphanopus intermedius,Aphanopus carbo",
-                            ifelse(asfis$latinName=="Argentina spp.","Argentina spp,Argentina sphyraena,Argentina silus,Argentina kagoshimae,Argentina elongata",
+                            ifelse(asfis$latinName=="Anarhichas spp", "Anarhichas spp,Anarhichas minor,Anarhichas denticulatus,Anarhichas lupus", 
+                            ifelse(asfis$latinName=="Aphanopus spp", "Aphanopus spp,Aphanopus intermedius,Aphanopus carbo",
+                            ifelse(asfis$latinName=="Argentina spp","Argentina spp,Argentina sphyraena,Argentina silus,Argentina kagoshimae,Argentina elongata",
                             ifelse(asfis$latinName=="Aristeomorpha foliacea", "Aristaeomorpha foliacea",
-                            ifelse(asfis$latinName=="Beryx spp.", "Beryx spp,Beryx splendens,Beryx decadactylus",
-                            ifelse(asfis$latinName=="Illex spp., Todarodes spp.", "Illex spp,Todarodes spp,Illex oxygonius,Illex argentinus,Illex coindetii,Illex illecebrosus,Todarodes angolensis,Todarodes pacificus,Todarodes filippovae,Todarodes sagittatus",
+                            ifelse(asfis$latinName=="Beryx spp", "Beryx spp,Beryx splendens,Beryx decadactylus",
+                            ifelse(asfis$latinName=="Illex spp, Todarodes spp.", "Illex spp,Todarodes spp,Illex oxygonius,Illex argentinus,Illex coindetii,Illex illecebrosus,Todarodes angolensis,Todarodes pacificus,Todarodes filippovae,Todarodes sagittatus",
                             ifelse(asfis$latinName=="Capros aper", "Capros aper,Caproidae",       
-                            ifelse(asfis$latinName=="Pandalus spp.", "Pandalus spp,Pandalus amplus,Pandalus nipponensis,Pandalus kessleri,Pandalus goniurus,Pandalus danae,Pandalus montagui,Pandalus jordani,Pandalus borealis,Pandalus platyceros,Pandalus hypsinotus",
+                            ifelse(asfis$latinName=="Pandalus spp", "Pandalus spp,Pandalus amplus,Pandalus nipponensis,Pandalus kessleri,Pandalus goniurus,Pandalus danae,Pandalus montagui,Pandalus jordani,Pandalus borealis,Pandalus platyceros,Pandalus hypsinotus",
                             ifelse(asfis$latinName=="Scomber colias", "Scomber japonicus",
-                            ifelse(asfis$latinName=="Scomber spp.", "Scomber spp,Scomber australasicus,Scomber scombrus,Scomber japonicus",
+                            ifelse(asfis$latinName=="Scomber spp", "Scomber spp,Scomber australasicus,Scomber scombrus,Scomber japonicus",
                             ifelse(asfis$latinName=="Solea vulgaris","Solea solea",
                             ifelse(asfis$latinName=="Trigla lucerna", "Chelidonichthys lucerna",
                             ifelse(asfis$latinName=="Trisopterus esmarki", "Trisopterus esmarkii",
-                            ifelse(asfis$latinName=="Trisopterus spp.", "Trisopterus spp,Trisopterus luscus,Trisopterus minutus,Trisopterus esmarkii",
+                            ifelse(asfis$latinName=="Trisopterus spp", "Trisopterus spp,Trisopterus luscus,Trisopterus minutus,Trisopterus esmarkii",
                                   as.character(asfis$latinName))))))))))))))))
 
 asfis$latinComm<-ifelse(is.na(asfis$X3A_CODE),"latinName_old not in ASFIS", "NA")
