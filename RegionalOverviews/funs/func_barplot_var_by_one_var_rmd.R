649e70b2a512a565cf0fe009222e1def2f0e729e
Kasia-MIR <kkrakowka@mir.local>
2020-04-22 12:31:51 +0200

    Update func_barplot_var_by_one_var_rmd.R

--- RegionalOverviews/funs/func_barplot_var_by_one_var_rmd.R
+++ RegionalOverviews/funs/func_barplot_var_by_one_var_rmd.R
@@ -29,10 +29,10 @@ barplot_var_by_one_var <- function(x,  Var, var1, tapply_type, type_of_threshold
 		# 2019-04-29: improve colour specification (do single colour barplots)
 		# 2019-05-08: changed output to list with values "table" and "plot"
 		# 2019-05-08: added argument save_plot_to_list (saves plot as second argument of final list)
-		
+    # 2020-04-07: added captions K.Krakówka
 		percent_Var <- round(sum(!is.na(x[,Var]))/dim(x)[1]*100,2)
 		percent_var1 <- round(sum(!is.na(x[,var1]))/dim(x)[1]*100,2)
-
+		source("../../funs/fun_rename.r")
 	# subset to threshold
 		if(is.na(type_of_threshold)) stop("check type_of_threshold")
 		if(!type_of_threshold == "NULL")
@@ -110,14 +110,40 @@ barplot_var_by_one_var <- function(x,  Var, var1, tapply_type, type_of_threshold
 	  barplot(t1, las=2, col=colour_scale, ylab = "", main = "", cex.names = graph_par$cex.x)
 	  if(title_root!="") title(main = paste(title_root,":",Var,"by", var1), line = 1.8) else title(main = paste(Var,"by", var1), line = 1.8)
 	  title(ylab=y_title, line = graph_par$ylab_line)
+	  var1<-rename_var1(var1)
+	  Var<-rename_var(Var)
 	  if(!type_of_threshold == "NULL")
 	  {
 	    title(main=paste("y:", percent_Var,"%; x:",percent_var1,"%; ",type_of_threshold,"(",value_of_threshold,")", sep=""), cex.main=0.9, line = 0.5)
+	    
+	    if(type_of_threshold == "cum_percent"){
+	      var1<-rename_var1(var1)
+	      Var<-rename_var(Var)
+	      caption<-paste(Var, ' by ',var1,'. ',var1,' form ',value_of_threshold, '% of the total ',Var,'.',sep='' )} 
+	    else { caption<-paste('caption 1' )}
+	    
+	    if(type_of_threshold == "main"){
+	      var1<-rename_var1(var1)
+	      Var<-rename_var(Var)
+	      caption<-paste(Var,' by ',value_of_threshold, ' main ', var1,' ( represent ',percent_var1, '% of all ',var1,')',sep='')
+	    }
+	    
 	  } else {
-	    title(main=paste("y:", percent_Var,"%; x:",percent_var1,"%; ","all_data", sep=""), cex.main=0.9, line = 0.5)
+	    if (percent_Var==100){
+	      if (tapply_type == "sum"){tapply_type = "Sum"}
+	      title(main=paste("y:", percent_Var,"%; x:",percent_var1,"%; ","all_data", sep=""), cex.main=0.9, line = 0.5)
+	      var1<-rename_var1(var1)
+	      Var<-rename_var(Var)
+	      caption<-paste(Var,' by ',var1,'for all data')}
+	    else{
+	      if (tapply_type == "sum"){tapply_type = "Sum"}
+	      var1<-rename_var1(var1)
+	      Var<-rename_var(Var)
+	      if (Var == "LandingWeight_1000ton"){Var="landings"}
+	      caption<-paste(tapply_type,' of ',Var,' by ',var1,' ( represent ',percent_Var, '% of all ',Var,')')}
 	  }
 		p <- recordPlot()
-		out<-list(table = data.frame(var1 = rownames(t1), Var = t1, row.names=NULL), plot = p)
+		out<-list(table = data.frame(var1 = rownames(t1), Var = t1, row.names=NULL), plot = p,caption)
 		dev.off()
 		} else {
 		  barplot(t1, las=2, col=colour_scale, ylab = "", main = "", cex.names = graph_par$cex.x)
@@ -129,7 +155,7 @@ barplot_var_by_one_var <- function(x,  Var, var1, tapply_type, type_of_threshold
 		  } else {
 		    title(main=paste("y:", percent_Var,"%; x:",percent_var1,"%; ","all_data", sep=""), cex.main=0.9, line = 0.5)
 		  }
-			out<-list(table = data.frame(var1 = rownames(t1), Var = t1, row.names=NULL), plot = NULL)
+			out<-list(table = data.frame(var1 = rownames(t1), Var = t1, row.names=NULL), plot = NULL,caption=NULL)
 			out
 			}
 	out
