a03accc3e5841c06b9f8e3a5b5e7bcad5fb81543
Kasia-MIR <kkrakowka@mir.local>
2020-04-28 13:43:20 +0200

    Update fun_rename.R

--- RegionalOverviews/funs/fun_rename.R
+++ RegionalOverviews/funs/fun_rename.R
@@ -6,7 +6,7 @@ rename_var1<-function(var1){
   if (var1=="FishingActivityLvl5") {var1="Metier Lvl5"}
   if (var1=="FishingActivityLvl6") {var1="Metier Lvl6"}
   if (var1=="FishingGround") {var1="fishing ground"}
-  if (var1=="Catch_group") {var1="catch group"}
+  if (var1=="Catch_group") {var1="Catch Group"}
   if (var1=="VesselLengthCategory"){var1="Vessel Length Category"}	  
   if (var1=="Harbour"){var1="Harbours"}	  
 return(var1)
@@ -19,7 +19,7 @@ rename_var2<-function(var2){
   if (var2=="FishingActivityLvl5") {var2="Metier Lvl5"}
   if (var2=="FishingActivityLvl6") {var2="Metier Lvl6"}
   if (var2=="FishingGround") {var2="fishing ground"}
-  if (var2=="Catch_group") {var2="catch group"}
+  if (var2=="Catch_group") {var2="Catch Group"}
   if (var2=="VesselLengthCategory"){var2="Vessel Length Category"}	
   if (var2=="LandingCountry"){var2="Landing Country"}
 
