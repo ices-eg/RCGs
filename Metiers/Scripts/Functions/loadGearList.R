da73b0e32fecd36e9b22baf76f590f1883b4791f
Maciej Adamowicz <madamowicz@mir.gdynia.pl>
2021-04-09 16:08:25 +0200

    Update loadGearList.R

--- Metiers/Scripts/Functions/loadGearList.R
+++ Metiers/Scripts/Functions/loadGearList.R
@@ -2,7 +2,7 @@ loadGearList <- function(url){
   print("Loading gear list ...")
   x <- data.table(read.xlsx(url, sheet = "formatted"))
   x[,Group:=tolower(Group)]
-  setnames(x, old = c("Code","Group","GEAR_Level6"), 
+  setnames(x, old = c("Code","Group","GEAR.Level6"), 
            new = c("gear_code", "gear_group","gear_level6"))
   if(any(duplicated(x$gear_code))){
     x<-x[!duplicated(x$gear_code)]
