71a8c916a6d0dd0850a8ae7510e145320118a194
Kasia-MIR <60775640+Kasia-MIR@users.noreply.github.com>
2021-04-19 14:27:16 +0200

    fleet register

---
+++ RegionalOverviews/funs/fun_theme_flextable.R
@@ -0,0 +1,18 @@
+theme_flextable <-function(data){
+  data %>%
+  t() %>%    
+  head() %>%
+  as.data.frame() %>% 
+  add_rownames() %>% 
+  flextable() %>%
+  theme_box() %>%
+  fontsize(part = "all", size = 6) %>%
+  bold(part="header") %>%
+  bold(j=1) %>%
+  bg(part="header",bg="#F3F9FF") %>%
+  color(part="header",color="black") %>%
+  color(j=1,part="header",color="#F3F9FF") %>%
+  bg(j=1, bg="#F3F9FF")%>%
+  align(align = "center", part = "all")->ft
+  return(ft)
+}
