72aebf9fef16a99824e2447a829263dc34ac5ca1
Kirsten Birch Håkansson <kibi@dtu.dk>
2021-09-01 13:51:59 +0200

    Adding code for country ref

--- NWPtools/table_2_1/00_rdb_code_ref.R
+++ NWPtools/table_2_1/00_rdb_code_ref.R
@@ -1,11 +1,20 @@
 
 
 # creating codes references from the RDB
+# Kirsten Birch Håkansson, DTU Aqua, Denmark
 
 path <- "Q:/mynd/RCM/RCGs/NWPtools/table_2_1/"
 
 load(paste(path,'RDB_All_Regions_CL_2009_2020_prepared_20210428.Rdata' ,sep = ''))
 
+# Areas ----
+
 rdb_areas <- unique(cl$Area)
 
 write.table(rdb_areas, paste0(path, "rdb_area_ref.csv"), sep = ";", row.names = F)
+
+# Countries ----
+
+rdb_ctry <- as.character(unique(cl$FlagCountry))
+
+write.table(rdb_ctry, paste0(path, "rdb_ctry_ref.csv"), sep = ";", row.names = F)
