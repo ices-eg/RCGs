750f339e364dd12c5466cc1239396a49c5367173
Kirsten Birch Håkansson <kibi@dtu.dk>
2021-09-01 09:17:32 +0200

    Nearly done script

--- NWPtools/table_2_1/01_table_1a_linkage_table_add_RDB_areas.R
+++ NWPtools/table_2_1/01_table_1a_linkage_table_add_RDB_areas.R
@@ -1,73 +1,244 @@
 
 
 # Adapting areas in the linkage table to the RDB areas / structure
+# A bit fast and dirty copy / paste coding
+# Kirsten Birch Håkansson, DTU Aqua, Denmark
 
 library(dplyr)
 library(tidyr)
 library(stringr)
+library(data.table)
 
 input_path <- "Q:/mynd/RCM/RCGs/NWPtools/table_2_1/NWP table 2.1/"
 output_path <- "Q:/mynd/RCM/RCGs/NWPtools/table_2_1/"
 
 
 linkage <- read.csv(file.path(input_path, 'EUMAP_Table_2_1_Linkage_EUROSTAT and EC_TAC_version_2021_final.csv'), sep = ";", header = T)
+rdb_areas <- arrange(read.csv(paste0(path, "rdb_area_ref.csv"), sep = ";"), x)
 
 names(linkage)
 
 linkage$areaRDB <- gsub(" ", "", linkage$areaBis)
+linkage$areaRDB <- paste0(",", gsub('_', '.', linkage$areaRDB), ",")
 
-check_1 <- subset(linkage, areaBis != areaRDB)
+
+linkage$areaRDB[linkage$region %in% c("Baltic Sea",
+                                      "North Sea and Eastern Arctic",
+                                      "North-East Atlantic")] <-
+  tolower(linkage$areaRDB[linkage$region %in% c("Baltic Sea",
+                                                "North Sea and Eastern Arctic",
+                                                "North-East Atlantic")])
+
+unique(linkage$areaRDB)
 
 # Baltic Sea / Baltic Sea (ICES areas 3b-d, FAO area 27)
 
-linkage$areaRDB[linkage$areaRDB == "27_3_D"] <-  "27_3_D,27_3_D_24,27_3_D_25,27_3_D_26,27_3_D_27,27_3_D_28,27_3_D_29,27_3_D_30,27_3_D_31,27_3_D_32"
+linkage$areaRDB <- gsub("27.3.d.28,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.3.d.28"], collapse = ","), ","), linkage$areaRDB)
 
-linkage$areaRDB <- gsub("27_3_D_28,", "27_3_D_28,27_3_D_28_1,27_3_D_28_2,", linkage$areaRDB)
+linkage$areaRDB <- gsub("27.3.d,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.3.d"], collapse = ","), ","), linkage$areaRDB)
 
-check_2 <- subset(linkage, areaBis != areaRDB)
+
+check_2 <- distinct(subset(linkage, region == "Baltic Sea"), area, areaBis, areaRDB)
 
 # North Sea and Eastern Arctic / Eastern Arctic, Norwegian Sea and Barents Sea (ICES areas 1, 2, FAO area 27)
 
-linkage$areaRDB[linkage$areaRDB == "27_1,27_2"] <- "27_1,27_1_A,27_1_B,27_2,27_2_A,27_2_A_1,27_2_A_2,27_2_B,27_2_B_1,27_2_B_2"
+linkage$areaRDB[linkage$areaBis == "27_4,27_7_D" &
+                  linkage$latinName == "Squalus acanthias"] <- "27.1,27.2," #Error
 
-linkage$areaRDB[linkage$areaRDB == "27_1,27_2,27_5_A,27_14"] <- "27_1,27_1_A,27_1_B,27_2,27_2_A,27_2_A_1,27_2_A_2,27_2_B,27_2_B_1,27_2_B_2,27_5_A,27_14,27_14_A,27_14_B,27_14_B_1,27_14_B_2"
+linkage$areaRDB <-
+  gsub("27.1,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.1" &
+                                           !(substr(rdb_areas$x, 5, 5) %in% c("0", "2", "4"))], collapse = ","), ","), linkage$areaRDB)
 
-linkage$areaRDB[linkage$areaRDB == "27_1,27_2,27_14"] <- "27_1,27_1_A,27_1_B,27_2,27_2_A,27_2_A_1,27_2_A_2,27_2_B,27_2_B_1,27_2_B_2,27_14,27_14_A,27_14_B,27_14_B_1,27_14_B_2"
+linkage$areaRDB <-
+  gsub("27.2.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.2.a"], collapse = ","), ","), linkage$areaRDB)
 
-linkage$areaRDB[linkage$areaRDB == "27_2"] <- "27_2,27_2_A,27_2_A_1,27_2_A_2,27_2_B,27_2_B_1,27_2_B_2"
+linkage$areaRDB <-
+  gsub("27.2,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.2"], collapse = ","), ","), linkage$areaRDB)
 
-linkage$areaRDB[linkage$areaRDB == "27_2_A"] <- "27_2_A,27_2_A_1,27_2_A_2"
 
-
-linkage$areaRDB[linkage$areaRDB == "27_4,27_7_D" &
-                  linkage$latinName == "Squalus acanthias"] <-
-  "27_1,27_1_A,27_1_B,27_2,27_2_A,27_2_A_1,27_2_A_2,27_2_B,27_2_B_1,27_2_B_2" # ERROR
-
-check_3 <- subset(linkage, areaBis != areaRDB)
 
 # North Sea and Eastern Arctic / North Sea and Eastern Channel (ICES areas 3a, 4 and 7d, FAO area 27)
 
-linkage$areaRDB[linkage$areaRDB == "27_3_A,27_4"] <- "27_3_A,27_3_A_20,27_3_A_21,27_4,27_4_A,27_4_B,27_4_C"
+linkage$areaRDB <-
+  gsub("27.3.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.3.a"], collapse = ","), ","), linkage$areaRDB)
 
-linkage$areaRDB[linkage$areaRDB == "27_4"] <- "27_4,27_4_A,27_4_B,27_4_C"
+linkage$areaRDB <-
+  gsub("27.4,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.4"], collapse = ","), ","), linkage$areaRDB)
 
-linkage$areaRDB[linkage$areaRDB == "27_3_A"] <- "27_3_A,27_3_A_20,27_3_A_21"
+linkage$areaRDB <-
+  gsub("27.7.d,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.d"], collapse = ","), ","), linkage$areaRDB)
 
-linkage$areaRDB[linkage$areaRDB == "27_3_A,27_4,27_7_D"] <- "27_3_A,27_3_A_20,27_3_A_21,27_4,27_4_A,27_4_B,27_4_C,27_7_D"
-
-linkage$areaRDB[linkage$areaRDB == "27_4,27_7_D"] <- "27_4,27_4_A,27_4_B,27_4_C,27_7_D"
-
-linkage$areaRDB[linkage$areaRDB == "27_7_D"] <- "27_7_D"
-
-linkage$areaRDB[linkage$areaRDB == "27_3_A-4_AB_NK" &
-                  linkage$latinName == "Gadus morhua"] <- "27_3_A_20" # ERROR
+linkage$areaRDB[linkage$area == "3aN" &
+                  linkage$latinName == "Gadus morhua"] <- "27.3.a.20" # ERROR
 
 linkage$areaRDB[linkage$area == "3aS" &
-                  linkage$latinName == "Gadus morhua"] <- "27_3_A_21" # ERROR
+                  linkage$latinName == "Gadus morhua"] <- "27.3.a.21" # ERROR
+
+linkage$areaRDB[linkage$area == "3aS" &
+                  linkage$latinName == "Pleuronectes platessa"] <- " " # Duplicate - already in the Baltic Sea
+
+linkage$areaRDB[linkage$area == "Union waters of 2a, 3a and 4" &
+                  linkage$latinName == "Solea solea"] <-
+  paste(rdb_areas$x[rdb_areas$x %like% "27.2.a" |
+                      rdb_areas$x %like% "27.4"], collapse = ",") # 3a a duplicate - already in the Baltic Sea
+
+check_3 <- distinct(subset(linkage, region == "North Sea and Eastern Arctic"), latinName, area, areaBis, areaRDB)
+
+# North-East Atlantic / North-East Atlantic and Western Channel (ICES areas 5, 6, 7 (excl. 7d), 8, 9, 10, 12 and 14, FAO area 27)
+
+linkage$areaRDB <-
+  gsub("27.5.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.5.a"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.5.b,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.5.b"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.5,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.5"], collapse = ","), ","), linkage$areaRDB)
 
 
+linkage$areaRDB <-
+  gsub("27.6.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.6.a"], collapse = ","), ","), linkage$areaRDB)
 
-check_4 <- subset(linkage, areaBis != areaRDB)
+linkage$areaRDB <-
+  gsub("27.6.b,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.6.b"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.6,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.6"], collapse = ","), ","), linkage$areaRDB)
+
+
+linkage$areaRDB <-
+  gsub("27.7.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.a"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.7.b,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.b"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.7.c,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.c"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.7.e,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.e"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.7.f,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.f"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.7.g,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.g"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.7.h,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.h"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.7.j,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.j"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.7.k,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7.k"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.7,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.7"], collapse = ","), ","), linkage$areaRDB)
+
+
+linkage$areaRDB <-
+  gsub("27.8.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.8.a"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.8.b,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.8.b"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.8.c,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.8.c"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.8.d,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.8.d"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.8.e,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.8.e"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.8,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.8"], collapse = ","), ","), linkage$areaRDB)
+
+
+linkage$areaRDB <-
+  gsub("27.9.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.9.a"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.9.b,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.9.b"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.9,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.9"], collapse = ","), ","), linkage$areaRDB)
+
+
+linkage$areaRDB <-
+  gsub("27.10.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.10.a"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.10.b,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.10.b"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.10,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.10"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.12.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.12.a"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.12.b,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.12.b"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.12.c,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.12.c"], collapse = ","), ","), linkage$areaRDB)
+linkage$areaRDB <-
+  gsub("27.12,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.12"], collapse = ","), ","), linkage$areaRDB)
+
+
+linkage$areaRDB <-
+  gsub("27.14.a,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.14.a"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.14.b,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.14.b"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub("27.14,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "27.14"], collapse = ","), ","), linkage$areaRDB)
+
+
+check_4 <- distinct(subset(linkage, region == "North-East Atlantic"), area, areaBis, areaRDB)
+
+# Other regions / North-West Atlantic (FAO area 21)
+
+linkage$areaRDB <-
+  gsub(",21.1,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "21.1"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub(",21.2,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "21.2"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub(",21.3,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "21.3"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub(",21.4,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "21.4"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub(",21.5,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "21.5"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub(",21.6,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "21.6"], collapse = ","), ","), linkage$areaRDB)
+
+linkage$areaRDB <-
+  gsub(",21,", paste0(paste(rdb_areas$x[rdb_areas$x %like% "21"], collapse = ","), ","), linkage$areaRDB)
+
+
+check_5 <- distinct(subset(linkage, region == "Other regions" & substr(areaBis, 1, 2) == "21"), area, areaBis, areaRDB)
+
+# Tests
+# Duplicated areas and species
+
+test1 <- select(linkage, region, latinName, area, areaRDB)
+
+areaRDB_split <- separate(data = test1, col = areaRDB, sep = ",", into = paste0("var", seq(1:100)))
+
+areaRDB_split_t <- gather(areaRDB_split, key = "areaRDB", value = "areaR", -region, -area, -latinName)
+
+areaRDB_split_t2 <- areaRDB_split_t[!is.na(areaRDB_split_t$areaR) & areaRDB_split_t$areaR != "", ]
+
+areaRDB_split_t2 <- mutate(arrange(areaRDB_split_t2, latinName, areaR), no = 1)
+
+areaRDB_split_t3 <- subset(mutate(group_by(areaRDB_split_t2, latinName, areaR), no_sum = sum(no)), no_sum > 1)
 
 # Output
 
