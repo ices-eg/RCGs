f6b22a12835891c7e410b30e55fdf1f9db281e2b
nmprista <31890653+nmprista@users.noreply.github.com>
2021-09-04 13:14:59 +0200

    added worms Family, Order, Class to rdb_spp

--- NWPtools/table_2_1/00_rdb_code_ref.R
+++ NWPtools/table_2_1/00_rdb_code_ref.R
@@ -2,8 +2,10 @@
 
 # creating codes references from the RDB
 # Kirsten Birch Håkansson, DTU Aqua, Denmark
+	# 2021-09-04: Nuno Prista added worms Family, Order, Class to rdb_spp
 
 library(dplyr)
+library("worrms")
 
 path <- "Q:/mynd/RCM/RCGs/NWPtools/table_2_1/"
 
@@ -21,8 +23,17 @@ rdb_ctry <- as.character(unique(cl$FlagCountry))
 
 write.table(rdb_ctry, paste0(path, "rdb_ctry_ref.csv"), sep = ";", row.names = F)
 
-# Countries ----
+# Species ----
 
 rdb_spp <- dplyr::distinct(cl, SpeciesAphiaID, Species, SpeciesDesc)
+
+rdb_spp$Family<-NA
+rdb_spp$Order<-NA
+rdb_spp$Class<-NA
+
+worms_extra<-data.frame(worrms::wm_classification_(id = c(rdb_spp$SpeciesAphiaID)))
+rdb_spp$Family<-worms_extra[worms_extra$rank=="Family","scientificname"][match(rdb_spp$SpeciesAphiaID, worms_extra[worms_extra$rank=="Family",]$id)]
+rdb_spp$Order<-worms_extra[worms_extra$rank=="Order","scientificname"][match(rdb_spp$SpeciesAphiaID, worms_extra[worms_extra$rank=="Order",]$id)]
+rdb_spp$Class<-worms_extra[worms_extra$rank=="Class","scientificname"][match(rdb_spp$SpeciesAphiaID, worms_extra[worms_extra$rank=="Class",]$id)]
 
 write.table(rdb_spp, paste0(path, "rdb_spp.csv"), sep = ";", row.names = F)
