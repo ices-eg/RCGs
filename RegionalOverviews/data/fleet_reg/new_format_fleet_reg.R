14901601870c8fbc8b490a4a78679c70b5e94507
Ana Fernandes <49688265+anacfernandes@users.noreply.github.com>
2023-03-07 15:03:31 +0000

    Update new_format_fleet_reg.R
    Some minor changes were done because of different date format.

--- RegionalOverviews/data/fleet_reg/new_format_fleet_reg.R
+++ RegionalOverviews/data/fleet_reg/new_format_fleet_reg.R
@@ -1,8 +1,8 @@
-for (ctry in c("BEL","DEU" ,"DNK","ESP","EST","FIN","FRA","IRL","LTU","LVA","NLD","POL","PRT","SWE")) # "GBR" - no data in FR 
+for (ctry in c("BEL","DEU" ,"DNK","ESP","EST","FIN","FRA","IRL","LTU","LVA","NLD","POL","PRT","SWE")) # "GBR" - no recent data in EU Fleet Register 
  #"BEL","DEU" ,"DNK","ESP","EST","FIN","FRA","GBR","IRL","LTU","LVA","NLD","POL","PRT","SWE"
 {
   #change year
-eu_fleet_register<-read.csv2(paste("D:\\RCGs\\RegionalOverviews\\data\\fleet_reg\\input\\2021\\FLEET-",ctry,".csv",sep = ""), sep=";", header=T, row.names=NULL, as.is=T)
+eu_fleet_register<-read.csv2(paste("../input/2022/FLEET-",ctry,".csv",sep = ""), sep=",", header=T, row.names=NULL, as.is=T) # check the separator used (',' or ';')
 names(eu_fleet_register)
 names(eu_fleet_register)<-c("Country_Code","CFR","UVI","Event_Code","Event_Start_Date","Event_End_Date",
                             "Registration_Nbr","Ext_Marking","Vessel_Name","Port_Code","IRCS","IRCS_Code",
@@ -13,15 +13,15 @@ names(eu_fleet_register)<-c("Country_Code","CFR","UVI","Event_Code","Event_Start
 
 #eu_fleet_register<-str_replace(eu_fleet_register,"[,]", "")
 a<-eu_fleet_register$Event_Start_Date
-a<- strptime(as.character(a), "%Y-%m-%d")
+a<- strptime(as.character(a), "%d/%m/%Y") # need to check the date format present in the file, to select the right one to put here (e.g. "%Y-%m-%d", "%d/%m/%Y", other)
 a<- format(a, "%Y%m%d")
 eu_fleet_register$Event_Start_Date<-as.numeric(a)
 
 a<-eu_fleet_register$Event_End_Date
-a<- strptime(as.character(a), "%Y-%m-%d")
+a<- strptime(as.character(a), "%d/%m/%Y") # same as for the 'Event_Start_Date'
 a<- format(a, "%Y%m%d")
 eu_fleet_register$Event_End_Date<-as.numeric(a)
 #change year 
-write.csv2(eu_fleet_register, file=paste("D:\\RCGs\\RegionalOverviews\\data\\fleet_reg\\output\\2021\\",ctry,"_export.csv",sep=""), row.names=F)	
+write.csv2(eu_fleet_register, file=paste("../output/2022/",ctry,"_export.csv", sep=""), row.names=F)	
 }
 
