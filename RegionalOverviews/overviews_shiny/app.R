ab01e0b3fb7982fec3998525ec165c739e7f804b
Eros Quesada <eros.quesada@slu.se>
2023-05-04 12:29:38 +0200

    Add launch file

---
+++ RegionalOverviews/overviews_shiny/app.R
@@ -0,0 +1,5 @@
+## --------------------------------
+#  Launch file
+## --------------------------------
+## This script launch the application once user interface and server are defined. 
+shinyApp(ui, server)
