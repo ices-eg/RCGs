077efcb049b143e32330ed6b8b998960f11516e1
Marta Suska <msuska@mir.gdynia.pl>
2024-01-11 06:41:53 +0100

    New html structure of the annual rdbes overview

---
+++ RegionalOverviews/overviews_reports/fisheries_overviews_RDBES/annual_overview_RDBES.R
@@ -0,0 +1,16 @@
+params <- list(
+  year = 2023,
+  region = 'Baltic',
+  logo_path = file.path("../../overviews_shiny/www/logo RCG BALTIC.PNG"),
+  data_dir = 'your path here' 
+  
+)
+
+rmdReport <- file.path("RegionalOverviews/overviews_reports/fisheries_overviews_RDBES/annual_overview_RDBES_template.Rmd")
+
+rmarkdown::render(
+  rmdReport,
+  params = params,
+  envir = new.env(parent = globalenv()),
+  encoding = 'UTF-8'
+)
