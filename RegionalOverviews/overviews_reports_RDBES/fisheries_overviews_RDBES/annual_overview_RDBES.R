4fc2fb34a4484e9c2f1c733b1a5e71f32c95f1d6
Marta Suska <msuska@mir.gdynia.pl>
2024-01-30 12:10:08 +0100

    Update annual_overview_RDBES.R

--- RegionalOverviews/overviews_reports_RDBES/fisheries_overviews_RDBES/annual_overview_RDBES.R
+++ RegionalOverviews/overviews_reports_RDBES/fisheries_overviews_RDBES/annual_overview_RDBES.R
@@ -5,16 +5,18 @@ params <- list(
   year = 2023,
   region = 'Baltic',
   logo_path = file.path("../../overviews_shiny/www/logo RCG BALTIC.PNG"),
-  data_dir = 'your path here', 
+  data_dir = '../../data_RDBES/002_prepared/20240129/RCG_BA', 
+  CLfileName = 'RDBES_RCG_BA_CL_2021_2021_prepared_20240129',
+  CEfileName = 'RDBES_RCG_BA_CE_2021_2021_prepared_20240129',
   RDBES_download_date = '01/01/2000'
   
 )
-
-rmdReport <- file.path("RegionalOverviews/overviews_reports/fisheries_overviews_RDBES/annual_overview_RDBES_template.Rmd")
+rmdReport <- file.path("RegionalOverviews/overviews_reports_RDBES/fisheries_overviews_RDBES/annual_overview_RDBES_template.Rmd")
 
 rmarkdown::render(
   rmdReport,
   params = params,
+  output_file = paste0('AnnualOverview_', params$year ,'_', params$region, '.html'),
   envir = new.env(parent = globalenv()),
   encoding = 'UTF-8'
 )
