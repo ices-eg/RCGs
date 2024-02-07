8a5994fd9f8aac407c2c39be2071d5ba2ba80f67
Eros Quesada <eros.quesada@slu.se>
2024-02-07 15:57:31 +0100

    Script to load all the needed libraries

---
+++ RegionalOverviews/overviews_reports_RDBES/fisheries_overviews_RDBES/scripts/loadLibraries.R
@@ -0,0 +1,41 @@
+###################################################################
+# loadLibraries
+###################################################################
+#
+# This script loads the libraries needed for the generation of the 
+# annual RDBES overview. 
+# 
+###################################################################
+# Authors: 
+# - Eros Quesada [first draft] 
+# 
+# Dev. notes: 
+# 
+# - 20240207: Created based on annual_overview_RDBES.R
+#
+###################################################################
+
+# Print start message
+cat("[2]    Loading libraries")
+
+# Empty warnings from previous code
+assign("last.warning", NULL, envir = baseenv()) # Credits: https://stackoverflow.com/questions/5725106/r-how-to-clear-all-warnings
+
+## Load libraries
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(car))
+suppressPackageStartupMessages(library(ggrepel))
+suppressPackageStartupMessages(library(knitr))
+suppressPackageStartupMessages(library(kableExtra))
+suppressPackageStartupMessages(library(rnaturalearth))
+suppressPackageStartupMessages(library(magrittr))
+suppressPackageStartupMessages(library(plotly))
+suppressPackageStartupMessages(library(crayon))
+
+# Print end message
+if(is_empty(warnings())){
+  cat("\n")
+  cat(green('       \u2713'), paste0(" - Completed")) 
+  cat("\n")
+} 
